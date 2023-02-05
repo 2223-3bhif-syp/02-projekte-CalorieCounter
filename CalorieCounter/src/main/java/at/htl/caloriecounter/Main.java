99907741f2401924b066a0dc8c5033e43c5ca2fb
Austromo <moritz.gruber.linz@gmail.com>
2023-02-05 17:39:53 +0100

    init project

---
+++ CalorieCounter/src/main/java/at/htl/caloriecounter/Main.java
@@ -0,0 +1,7 @@
+package at.htl.caloriecounter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ CalorieCounter/src/main/java/at/htl/caloriecounter/database/SqlScript.java
@@ -0,0 +1,7 @@
+package at.htl.caloriecounter.database;
+
+public enum SqlScript {
+    DROP,
+    CREATE,
+    INSERT
+}
