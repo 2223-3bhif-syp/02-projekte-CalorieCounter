b21fe9af9ee95ad742fc2a85040ebd003b55a086
Austromo <moritz.gruber.linz@gmail.com>
2023-02-26 21:57:03 +0100

    fixed UserTest

--- CalorieCounter/src/test/java/at/htl/caloriecounter/entity/UserTest.java
+++ CalorieCounter/src/test/java/at/htl/caloriecounter/entity/UserTest.java
@@ -3,6 +3,7 @@ package at.htl.caloriecounter.entity;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +14,7 @@ public class UserTest {
 
 
         // act
-        User user = new User("m.muster@gmail.com", "musti", "1234", 80.3, 176, new Goal(70.0, LocalDate.of(2023, 3, 20)));
+        User user = new User("m.muster@gmail.com", "musti", "1234", 80.3, 176, new Goal(70.0, LocalDateTime.of(2023, 3, 20, 0, 0)));
 
         // assert
         assertThat(user.getWeight()).isEqualTo(80.3);
