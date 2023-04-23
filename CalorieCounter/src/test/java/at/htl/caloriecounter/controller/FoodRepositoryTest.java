a2314695d5eba7c50b1ac454049ae80900c30abe
StroschneiderFabian <stro.fabian@gmail.com>
2023-04-23 10:50:16 +0200

    23kalorien-19 (stable): can insert a single food

--- CalorieCounter/src/test/java/at/htl/caloriecounter/controller/FoodRepositoryTest.java
+++ CalorieCounter/src/test/java/at/htl/caloriecounter/controller/FoodRepositoryTest.java
@@ -4,17 +4,16 @@ import at.htl.caloriecounter.database.SqlRunner;
 import at.htl.caloriecounter.database.SqlScript;
 import at.htl.caloriecounter.entity.Food;
 import org.assertj.db.type.Table;
+import org.junit.jupiter.api.Test;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import javax.sql.DataSource;
-
 import static org.assertj.db.api.Assertions.assertThat;
 import static org.assertj.db.output.Outputs.output;
 
 public class FoodRepositoryTest {
     FoodRepository foodRepository = new FoodRepository();
+    DataSource dataSource = Database.getDataSource();
 
     @BeforeAll
     static void createTables() {
@@ -22,20 +21,21 @@ public class FoodRepositoryTest {
     }
 
     @Test
-    void insertFood_ok() {
+    void testInsertFood() {
         // arrange
-        DataSource ds = Database.getDataSource();
-        Food food = new Food("tomate", 21.0);
-        Table table = new Table(ds, "CC_FOOD");
+
+        Food food = new Food("tomato", 21.0);
 
         // act
         foodRepository.save(food);
 
         // assert
+        Table table = new Table(dataSource, "CC_FOOD");
         output(table).toConsole();
         assertThat(table).exists()
                 .row(0)
-                .column("F_NAME").value().isEqualTo("tomate");
+                .column("F_NAME").value().isEqualTo("tomato")
+                .column("F_CALORIES").value().isEqualTo(21.0);
     }
 
     @AfterAll
