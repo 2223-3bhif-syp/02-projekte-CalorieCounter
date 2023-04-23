192ce45cad014f302bd90666a2f0508a4d872aba
StroschneiderFabian <stro.fabian@gmail.com>
2023-04-23 11:19:34 +0200

    23kalorien-20 (stable): can insert a multiple foods

--- CalorieCounter/src/test/java/at/htl/caloriecounter/controller/FoodRepositoryTest.java
+++ CalorieCounter/src/test/java/at/htl/caloriecounter/controller/FoodRepositoryTest.java
@@ -4,10 +4,10 @@ import at.htl.caloriecounter.database.SqlRunner;
 import at.htl.caloriecounter.database.SqlScript;
 import at.htl.caloriecounter.entity.Food;
 import org.assertj.db.type.Table;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import javax.sql.DataSource;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+
 import static org.assertj.db.api.Assertions.assertThat;
 import static org.assertj.db.output.Outputs.output;
 
@@ -15,13 +15,13 @@ public class FoodRepositoryTest {
     FoodRepository foodRepository = new FoodRepository();
     DataSource dataSource = Database.getDataSource();
 
-    @BeforeAll
-    static void createTables() {
+    @BeforeEach
+    void createTables() {
         SqlRunner.runScript(SqlScript.CREATE);
     }
 
     @Test
-    void testInsertFood() {
+    void test_insert_food() {
         // arrange
 
         Food food = new Food("tomato", 21.0);
@@ -38,8 +38,31 @@ public class FoodRepositoryTest {
                 .column("F_CALORIES").value().isEqualTo(21.0);
     }
 
-    @AfterAll
-    static void dropTables() {
+    @Test
+    void test_insert_multiply_food() {
+        // arrange
+
+        Food food1 = new Food("orange", 47.0);
+        Food food2 = new Food("banana", 89.0);
+
+        // act
+        foodRepository.save(food1);
+        foodRepository.save(food2);
+
+        // assert
+        Table table = new Table(dataSource, "CC_FOOD");
+        output(table).toConsole();
+        assertThat(table).exists().hasNumberOfRows(2)
+                .row(0)
+                .column("F_NAME").value().isEqualTo("orange")
+                .column("F_CALORIES").value().isEqualTo(47.0)
+                .row(1)
+                .column("F_NAME").value().isEqualTo("banana")
+                .column("F_CALORIES").value().isEqualTo(89.0);
+    }
+
+    @AfterEach
+    void dropTables() {
         SqlRunner.runScript(SqlScript.DROP);
     }
 }
