43f7999e5f73c88f8d4644fb76c5a38731fe73cc
StroschneiderFabian <stro.fabian@gmail.com>
2023-04-23 21:51:51 +0200

    23kalorien-31 (stable): delete  workout

--- CalorieCounter/src/test/java/at/htl/caloriecounter/controller/WorkoutRepositoryTest.java
+++ CalorieCounter/src/test/java/at/htl/caloriecounter/controller/WorkoutRepositoryTest.java
@@ -23,7 +23,7 @@ public class WorkoutRepositoryTest {
     }
 
     @Test
-    void insertWorkout_ok() {
+    void test_insert_workout() {
         // arrange
         User user = new User("f.stro@example.com", "f.stro", "123", 70, 175);
         Workout workout = new Workout("Laufen", 250, 1, user);
@@ -41,6 +41,24 @@ public class WorkoutRepositoryTest {
                 .column("W_NAME").value().isEqualTo(workout.getName())
                 .column("W_CALORIES").value().isEqualTo((workout.getCalories()))
                 .column("W_DURATION").value().isEqualTo(workout.getDuration());
+    }
+
+    @Test
+    void test_delete_workout() {
+        // arrange
+        User user = new User("f.stro@example.com", "f.stro", "123", 70, 175);
+        Workout workout = new Workout("Laufen", 250, 1, user);
+
+        // act
+        userRepository.save(user);
+        workoutRepository.save(workout);
+
+        workoutRepository.delete(workout.getId());
+
+        // assert
+        Table table = new Table(dataSource, "CC_WORKOUT");
+        output(table).toConsole();
+        assertThat(table).isEmpty();
     }
 
     @AfterEach
