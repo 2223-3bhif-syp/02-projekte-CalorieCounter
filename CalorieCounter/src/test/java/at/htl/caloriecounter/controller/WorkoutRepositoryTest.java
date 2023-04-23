ca9f3168fb285e28f9096cae0ed9afc80c6f9486
StroschneiderFabian <stro.fabian@gmail.com>
2023-04-23 21:54:00 +0200

    23kalorien-30 (stable): update  workout

--- CalorieCounter/src/test/java/at/htl/caloriecounter/controller/WorkoutRepositoryTest.java
+++ CalorieCounter/src/test/java/at/htl/caloriecounter/controller/WorkoutRepositoryTest.java
@@ -61,6 +61,31 @@ public class WorkoutRepositoryTest {
         assertThat(table).isEmpty();
     }
 
+    @Test
+    void test_update_workout() {
+        // arrange
+        User user = new User("f.stro@example.com", "f.stro", "123", 70, 175);
+        Workout workout = new Workout("Laufen", 250, 1, user);
+
+        // act
+        userRepository.save(user);
+        workoutRepository.save(workout);
+        workout.setDuration(2);
+        workout.setCalories(500);
+
+        workoutRepository.update(workout);
+
+        // assert
+        Table table = new Table(dataSource, "CC_WORKOUT");
+        output(table).toConsole();
+        assertThat(table).exists()
+                .row(0)
+                .column("W_U_ID").value().isEqualTo(workout.getId())
+                .column("W_NAME").value().isEqualTo(workout.getName())
+                .column("W_CALORIES").value().isEqualTo((workout.getCalories()))
+                .column("W_DURATION").value().isEqualTo(workout.getDuration());
+    }
+
     @AfterEach
     void dropTables() {
         SqlRunner.runScript(SqlScript.DROP);
