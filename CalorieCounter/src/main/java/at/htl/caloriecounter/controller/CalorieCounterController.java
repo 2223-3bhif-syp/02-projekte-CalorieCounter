ba9641c47ec83a2a7ec14c947c463dabeeb8df18
markSuuS <m.breinesberger@students.htl-leonding.ac.at>
2023-06-09 00:27:09 +0200

    23kalorien-48 improved Menu FXML, first handling methonds in ctrler

--- CalorieCounter/src/main/java/at/htl/caloriecounter/controller/CalorieCounterController.java
+++ CalorieCounter/src/main/java/at/htl/caloriecounter/controller/CalorieCounterController.java
@@ -1,10 +1,99 @@
 package at.htl.caloriecounter.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class CalorieCounterController {
+    //region Buttons
+    @FXML
+    private Button trackCaloriesBtn;
+
+    @FXML
+    private Button statisticsBtn;
+
+    @FXML
+    private Button improveGoalsBtn;
+
+    @FXML
+    private Button settingsBtn;
+
+    @FXML
+    private Button fmaBtn;
+
+    @FXML
+    private Button csmBtn;
+
+    @FXML
+    private Button insertOtherValuesBtn;
+
+    @FXML
+    private Button toolbarSexBtn;
+
+    @FXML
+    private Button toolbarAgeBtn;
+
+    @FXML
+    private Button toolbarWeightBtn;
+
+    @FXML
+    private Button toolbarHeightBtn;
+
+    @FXML
+    private Button toolbarDesiredWeightBtn;
+
+    //endregion
+
     @FXML
     private void initialize() {
         //TODO
+    }
+
+    @FXML
+    private void trackCaloriesPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void statisticsPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void improveGoalsPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void settingsPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void fmaPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void csmPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void insertOtherValuesPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void toolbarSexPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void toolbarAgePressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void toolbarWeightPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void toolbarHeightPressed(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void toolbarDesiredWeightPressed(ActionEvent actionEvent) {
     }
 }
