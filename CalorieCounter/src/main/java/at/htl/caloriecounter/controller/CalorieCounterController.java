3057e6392010963f187956ffeb8f30095d114262
markSuuS <m.breinesberger@students.htl-leonding.ac.at>
2023-06-11 23:19:43 +0200

    23-kalorien-48 User property settings with TextInputDialog and ChoiceDialog

--- CalorieCounter/src/main/java/at/htl/caloriecounter/controller/CalorieCounterController.java
+++ CalorieCounter/src/main/java/at/htl/caloriecounter/controller/CalorieCounterController.java
@@ -3,6 +3,9 @@ package at.htl.caloriecounter.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.TextInputDialog;
+import java.util.Optional;
 
 public class CalorieCounterController {
     //region Buttons
@@ -79,21 +82,71 @@ public class CalorieCounterController {
 
     @FXML
     private void toolbarSexPressed(ActionEvent actionEvent) {
+        ChoiceDialog sexDialog = new ChoiceDialog("", "male", "female", "other");
+        sexDialog.setTitle("Sex settings");
+        sexDialog.setHeaderText("Choose your gender:");
+        sexDialog.setContentText("Sex: ");
+
+        Optional<String> result = sexDialog.showAndWait();
+
+        result.ifPresent(sex -> {
+            //now change gender in user
+        });
     }
 
     @FXML
     private void toolbarAgePressed(ActionEvent actionEvent) {
+        TextInputDialog ageDialog = new TextInputDialog();
+        ageDialog.setTitle("Age settings");
+        ageDialog.setHeaderText("Enter your age:");
+        ageDialog.setContentText("Age: ");
+
+        Optional<String> result = ageDialog.showAndWait();
+
+        result.ifPresent(age -> {
+            //now change the age in current user
+        });
     }
 
     @FXML
     private void toolbarWeightPressed(ActionEvent actionEvent) {
+        TextInputDialog weightDialog = new TextInputDialog();
+        weightDialog.setTitle("Weight settings");
+        weightDialog.setHeaderText("Enter your weight:");
+        weightDialog.setContentText("weight: ");
+
+        Optional<String> result = weightDialog.showAndWait();
+
+        result.ifPresent(weight -> {
+            //now change the weight in current user
+        });
     }
 
     @FXML
     private void toolbarHeightPressed(ActionEvent actionEvent) {
+        TextInputDialog heightDialog = new TextInputDialog();
+        heightDialog.setTitle("Height settings");
+        heightDialog.setHeaderText("Enter your height:");
+        heightDialog.setContentText("Height: ");
+
+        Optional<String> result = heightDialog.showAndWait();
+
+        result.ifPresent(height -> {
+            //now change the height in current user
+        });
     }
 
     @FXML
     private void toolbarDesiredWeightPressed(ActionEvent actionEvent) {
+        TextInputDialog desiredWeightDialog = new TextInputDialog();
+        desiredWeightDialog.setTitle("desired weight settings");
+        desiredWeightDialog.setHeaderText("Enter your desired weight:");
+        desiredWeightDialog.setContentText("Desired Weight: ");
+
+        Optional<String> result = desiredWeightDialog.showAndWait();
+
+        result.ifPresent(desiredWeight -> {
+            //now change the desired weight in current user
+        });
     }
 }
